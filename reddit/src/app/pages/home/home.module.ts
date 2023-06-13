7060ee1cbbe7d8585369aa8801840205b4d5b793
Luca Sernicola <125129925+Bjoorng@users.noreply.github.com>
2023-06-13 09:24:09 +0200

    added ripple

--- reddit/src/app/pages/home/home.module.ts
+++ reddit/src/app/pages/home/home.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -18,6 +19,7 @@ import { HomeComponent } from './home.component';
     MatCardModule,
     MatSidenavModule,
     MatIconModule,
+    MatRippleModule
   ]
 })
 export class HomeModule { }
